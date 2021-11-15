1ea1ba512756210afa08db484a235ba58a345ece
niki <hootan09@gmail.com>
2021-11-15 16:43:29 +0330

    \Deploy Next.js to gh-pages"

---
+++ _next/static/Vke9o3_TNg39-4UOzABDY/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002Farticles","\u002F","\u002Fprojects","\u002Fsettings","\u002Fcontact","\u002Fgithub"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
